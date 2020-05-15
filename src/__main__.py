d43c9e9a5864bc26bb4ebbdfcd519b379f820981
OTakumi <takumi.o.0410@gmail.com>
2020-05-15 14:33:08 +0900

    add qatm_torch.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,17 @@
 # !/usr/bin/env python
 
+from pathlib improt Path
 import cv2
+import torch
+import torchvision
+from torchvision import models, transforms, utils
+import argparse
+from utils import *
+
+# import functions and classes from qatm_torch.py
+import ast
+import types
+import sys
 
 def main():
     print("Hello World")
