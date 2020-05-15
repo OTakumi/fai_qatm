7f0303a5b7828506410d9ca7eac386150b9d81fe
OTakumi <takumi.o.0410@gmail.com>
2020-05-15 15:49:08 +0900

    delete init file

--- src/qatm_torch.py
+++ src/qatm_torch.py
@@ -1,4 +1,24 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import models, transforms, utils
+import copy
+from utils import *
+
+class ImageDataSet(torch.utils.data.Dataset):
+    def __init__(self, template_dir_path, image_name, thresh_csv=None, transform=None):
+        self.transform = transform
+        if not self.transform:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                )
+            ])
