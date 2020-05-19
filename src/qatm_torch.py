d7a7b3742d068076acb55cca3a1d349e00c97e7d
OTakumi <takumi.o.0410@gmail.com>
2020-05-19 18:12:02 +0900

    add featex class

--- src/qatm_torch.py
+++ src/qatm_torch.py
@@ -58,3 +58,41 @@ def __getitem__(self, idx):
 template_dir = 'template/'
 image_path = 'sample/sample.jpg'
 dataset = ImageDataSet(Path(template_dir), image_path, thresh_csv='thresh_template.csv')
+
+class Featex():
+    '''
+    Extract Feature
+    '''
+    def __init__(self, model, use_cuda):
+        self.use_cuda = use_cuda
+        self.feature1 = None
+        self.feature2 = None
+        self.model = copy.deepcopy(model.eval())
+        self.model = self.model[:17]
+        for param in self.model.parameters():
+            param.requires_grad = False
+        if self.use_cuda:
+            self.model = self.model.cuda()
+        self.model[2].register_forward_hook(self.save_feature1)
+        self.model[16].register_forward_hook(self.save_feature2)
+    
+    def save_feature1(self, module, input, output):
+        self.feature1 = output.detach()
+    
+    def save_feature2(self, module, input, output):
+        self.feature2 = output.detach()
+
+    def __call__(self, input, mode='big'):
+        if self.use_cuda:
+            input = input.cuda()
+            = self.model(input)
+        if mode=='big':
+            # resize feature1 to the same size of feature2
+            self.feature1 = F.interpolate(self.feature1, size=(self.feature2.size()[2], self.feature2.size()[3]), 
+            model='bilinear', align_corners=True)
+        else:
+            # resize feature2 to the same size of feature1
+            self.feature2 = F.interpolate(self.feature2, size=(self.feature1.size()[2], self.feature1.size()[3]), 
+            model='bilinear', align_corners=True)
+        
+        return torch.cat((self.feature1, self.feature2), dim=1)
