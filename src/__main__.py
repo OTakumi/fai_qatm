a999c6a949d8a793b871aaf25a74f61a6d468935
OTakumi <takumi.o.0410@gmail.com>
2020-06-24 17:57:15 +0900

    update

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python
 
-from pathlib improt Path
+from pathlib import Path
 import cv2
 import torch
 import torchvision
@@ -13,9 +13,39 @@ import ast
 import types
 import sys
 
-def main():
-    print("Hello World")
+with open("src/qatm_torch.py") as f:
+       p = ast.parse(f.read())
 
+for node in p.body[:]:
+    if not isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.Import, ast.ImportFrom)):
+        p.body.remove(node)
+
+module = types.ModuleType("mod")
+code = compile(p, "mod.py", 'exec')
+sys.modules["mod"] = module
+exec(code,  module.__dict__)
+
+from mod import *
+# -
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='QATM Pytorch Implementation')
+    parser.add_argument('--cuda', action='store_true')
+    parser.add_argument('-s', '--sample_image', default='sample/sample1.jpg')
+    parser.add_argument('-t', '--template_images_dir', default='template/')
+    parser.add_argument('--alpha', type=float, default=25)
+    parser.add_argument('--thresh_csv', type=str, default='thresh_template.csv')
+    args = parser.parse_args()
+    
+    template_dir = args.template_images_dir
+    image_path = args.sample_image
+    dataset = ImageDataset(Path(template_dir), image_path, thresh_csv='thresh_template.csv')
+    
+    print("define model...")
+    model = CreateModel(model=models.vgg19(pretrained=True).features, alpha=args.alpha, use_cuda=args.cuda)
+    print("calculate score...")
+    scores, w_array, h_array, thresh_list = run_multi_sample(model, dataset)
+    print("nms...")
+    boxes, indices = nms_multi(scores, w_array, h_array, thresh_list)
+    _ = plot_result_multi(dataset.image_raw, boxes, indices, show=False, save_name='result.png')
+    print("result.png was saved")
